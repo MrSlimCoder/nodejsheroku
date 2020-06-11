c33c4c8ac9c749bd4cc2bffd27d4e222f34cf82b
vivekbykea <vivek.anand@bykea.com>
2020-06-11 20:17:36 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT | 3000;
+
+app.get("/",(req,res) =>
+{
+    res.send("Hello World")
+})
+
+app.listen(PORT, (err) =>
+{
+    if(err) console.log(err);
+    else console.log("Server is Running! on PORT "+PORT);
+})
