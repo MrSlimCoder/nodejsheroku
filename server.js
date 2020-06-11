4f2d78035c6e1d2c5e6d2b6d899a8becadf484d9
vivekbykea <vivek.anand@bykea.com>
2020-06-11 20:43:02 +0500

    demo

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT | 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/",(req,res) =>
 {
