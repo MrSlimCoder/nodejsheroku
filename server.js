b8f49604103689761ae383da609ef27fba3a1842
vivekbykea <vivek.anand@bykea.com>
2020-06-11 21:08:09 +0500

    initial commit

--- server.js
+++ server.js
@@ -1,18 +1,1 @@
-const express = require("express");
-const bodyParser = require('body-parser');
-const app = express();
-const PORT = process.env.PORT | 3000;
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.get("/",(req,res) =>
-{
-    res.send("Hello World")
-})
-
-app.listen(PORT, (err) =>
-{
-    if(err) console.log(err);
-    else console.log("Server is Running! on PORT "+PORT);
-})
+console.log("Hello World");
